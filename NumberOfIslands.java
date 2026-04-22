4d90e7a74592a075d58bf5998bc520c9285bd171
Baskar2312 <baskar2312bass@gmail.com>
2026-04-22 13:48:54 +0530

    Add NumberOfIslands.java

---
+++ NumberOfIslands.java
@@ -0,0 +1,35 @@
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        System.out.println(new NumberOfIslands().numIslands(grid));  
+    }
+    public int numIslands(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(int r,int c,char[][] grid){
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]=='0'){
+            return;
+        }
+        grid[r][c]='0';
+        dfs(r+1,c,grid);
+        dfs(r-1,c,grid);
+        dfs(r,c+1,grid);
+        dfs(r,c-1,grid);
+    }
+}
